ccdc4b5d7a57b2fd6e0c6a4678a7c333415b0d8b
OscarKolodziejczyk <oscar.kolodziejczyk@mail.utoronto.ca>
2026-06-08 20:53:25 -0700

    Feature: Implemented transformation of data from raw CSVs to standardized Title, Description, and DataType columns for each data set (movies, books, and games)

---
+++ transform_data.py
@@ -0,0 +1,42 @@
+from extract_data import extract_data_from_azure
+import pandas as pd
+
+def clean_data():
+
+    # Collect our extracted dataframes dictionary
+    dfs = extract_data_from_azure()
+
+    # New dictionary that will hold our transformed data
+    cleaned_dfs = {}
+
+    # Transform all 3 CSVs to include only Title, Description, and DataType
+    # columns, with no missing values.
+
+    # Books:
+    if "raw_books.csv" in dfs.keys():
+        df_books = dfs['raw_books.csv'][['title', 'description']].dropna().copy()
+        df_books.insert(2, 'DataType', "Book")
+        df_books.rename(columns={'title': 'Title', 'description': 'Description'}, inplace=True)
+        cleaned_dfs['books'] = df_books
+    else:
+        print("Error, raw_books.csv does not exist")
+
+    # Movies:
+    if "raw_movies.csv" in dfs.keys():
+        df_movies = dfs['raw_movies.csv'][['Series_Title', 'Overview']].dropna()
+        df_movies.insert(2, 'DataType', "Movie")
+        df_movies.rename(columns={'Series_Title': 'Title', 'Overview': 'Description'}, inplace=True)
+        cleaned_dfs['movies'] = df_movies
+    else:
+        print("Error, raw_movies.csv does not exist")
+
+    # Video Games:
+    if "raw_games.csv" in dfs.keys():
+        df_games = dfs['raw_games.csv'][['Title', 'Summary']].dropna()
+        df_games.insert(2, 'DataType', "Game")
+        df_games.rename(columns={'Summary': 'Description'}, inplace=True)
+        cleaned_dfs['games'] = df_games
+    else:
+        print("Error, raw_games.csv does not exist")
+
+    return cleaned_dfs
